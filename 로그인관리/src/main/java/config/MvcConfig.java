be420d846e0a0376c03648d8b3af345ad9a0f579
pengnim <pengnim98@gmail.com>
2022-01-25 17:28:47 +0900

    이미지추가

--- 로그인관리/src/main/java/config/MvcConfig.java
+++ 로그인관리/src/main/java/config/MvcConfig.java
@@ -23,8 +23,8 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new 로그인확인자())
-		        .addPathPatterns("/**")
-		        .excludePathPatterns("/main")
+		        .addPathPatterns("/**") //모든 패턴 확인
+		        .excludePathPatterns("/main") // 단 /main, /login은 제외
 		        .excludePathPatterns("/login");		
 		//Test 할 것 -로그인 없이 /member 요청하면 -> 로그인 창이 뜨며 로그인을 요청함  
 		
